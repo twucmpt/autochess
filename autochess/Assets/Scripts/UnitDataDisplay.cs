99d4bd43ff56dd7871770bd64fa256d96b267585
SamDelaney <sam@thedelaneys.net>
2022-10-03 02:15:08 -0700

    working commit while trying to figure out UI formatting

--- autochess/Assets/Scripts/UnitDataDisplay.cs
+++ autochess/Assets/Scripts/UnitDataDisplay.cs
@@ -6,7 +6,7 @@ public class UnitDataDisplay : MonoBehaviour
 {
     public Shop shop;
     public int slot;
-    public TMPro.TMP_Text costText;
+    public TMPro.TMP_Text costText, nameText, healthText, powerText;
 
     void Awake() {
         shop.SlotUpdated.AddListener(OnSlotUpdated);
@@ -15,5 +15,8 @@ public class UnitDataDisplay : MonoBehaviour
     public void OnSlotUpdated(int slot) {
         Unit unit = shop.GetShopUnit(slot).GetComponent<Unit>();
         costText.SetText(unit.type.cost.ToString());
+        //nameText.SetText(unit.type.name.ToString());
+        //healthText.SetText(unit.type.stats.health.ToString());
+        powerText.SetText(unit.power.ToString());
     }
 }
