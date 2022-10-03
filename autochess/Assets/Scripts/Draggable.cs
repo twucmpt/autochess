db18831d3921b6a0dc15e856b677517a9713a4eb
Bit Flipz <114410587+BitFlipz@users.noreply.github.com>
2022-10-03 00:17:58 -0400

    Units are swappable

--- autochess/Assets/Scripts/Draggable.cs
+++ autochess/Assets/Scripts/Draggable.cs
@@ -26,6 +26,7 @@
     }
     if (!enabled) return;
     transform.position = GetCurrentMousePos();
+    if (GetComponent<Unit>().animator != null) GetComponent<Unit>().animator.SetBool("Walking", false);
  }
 
  
@@ -35,6 +36,8 @@
     pickedUp = false;
     var currentPosRounded = Vector2Int.RoundToInt(GetCurrentMousePos());
     Unit unit = GetComponent<Unit>();
+    Unit unitAtPos = GameManager.Instance.unitPositions.ContainsKey(currentPosRounded)? GameManager.Instance.unitPositions[currentPosRounded] : null;
+
     if (Graveyard.Instance.Contains(gameObject)) {
         if (currentPosRounded.x < GameManager.Instance.gridWidth / 2 && GameManager.Instance.AddUnit(currentPosRounded, gameObject)) {
             transform.position = new Vector3(currentPosRounded.x, currentPosRounded.y);
@@ -50,10 +53,12 @@
                 Bench.Instance.RemoveUnit(gameObject);
                 GameManager.Instance.PlaySFX(unit.type.GetSound("placement"));
             } else {
+                if (currentPosRounded.x < GameManager.Instance.gridWidth / 2 && unitAtPos != null && unitAtPos != unit && unitAtPos.CompareTag("Player")) {
+                    SwapUnits(unit, unitAtPos);
+                    return;
+                }
                 GameManager.Instance.PlaySFX(Resources.Load<AudioClip>("SFX/cantdothat"));
                 transform.position = originalPos;
-                
-                
             }
         }
     else {
@@ -61,8 +66,22 @@
             GameManager.Instance.RemoveUnit(unit);
             return;
         }
+        else {
+            var currentPosRoundedForBench = Vector2Int.RoundToInt(GetCurrentMousePos() + new Vector3(-1.5f, 0));
+            if (currentPosRoundedForBench.y == -1 && currentPosRoundedForBench.x >= 0 && currentPosRoundedForBench.x < 5) {
+                var unitAtBench = Bench.Instance.units[currentPosRoundedForBench.x];
+                if (unitAtBench != null) {
+                    SwapUnits(unit, unitAtBench.GetComponent<Unit>());
+                    return;
+                }
+            }
+        }
         if (GameManager.Instance.CheckValidPosition(currentPosRounded, tag) && currentPosRounded.x < GameManager.Instance.gridWidth / 2 && unit.MoveUnit(currentPosRounded)) transform.position = new Vector3(currentPosRounded.x, currentPosRounded.y);
         else {
+            if (currentPosRounded.x < GameManager.Instance.gridWidth / 2 && unitAtPos != null && unitAtPos != unit && unitAtPos.CompareTag("Player")) {
+                SwapUnits(unit, unitAtPos);
+                return;
+            }
             if (currentPosRounded.x != originalPos.x || currentPosRounded.y != originalPos.y) {
                     GameManager.Instance.PlaySFX(Resources.Load<AudioClip>("SFX/cantdothat"));
             }
@@ -70,6 +89,48 @@
         }
     }
     
+ }
+
+ private void SwapUnits(Unit unit1, Unit unit2) {
+    bool unit1Benched = Bench.Instance.Contains(unit1.gameObject);
+    bool unit2Benched = Bench.Instance.Contains(unit2.gameObject);
+    if (unit1Benched || unit2Benched) {
+        if (GameManager.Instance.currentPhase == GamePhase.Planning) {
+            GameManager.Instance.PlaySFX(Resources.Load<AudioClip>("SFX/cantdothat"));
+            return;
+        }
+        Unit benchedUnit = null;
+        Unit placedUnit = null;
+        if (unit1Benched) {
+            benchedUnit = unit1;
+            placedUnit = unit2;
+        }
+        else {
+            placedUnit = unit1;
+            benchedUnit = unit2;
+        }
+        GameManager.Instance.RemoveUnit(placedUnit);
+
+        // Add from bench
+        GameManager.Instance.AddUnit(placedUnit.gridPos, benchedUnit.gameObject);
+        benchedUnit.transform.position = new Vector3(placedUnit.gridPos.x, placedUnit.gridPos.y);
+        Bench.Instance.RemoveUnit(benchedUnit.gameObject);
+        GameManager.Instance.PlaySFX(unit1.type.GetSound("placement"));
+
+        // Add to bench
+        Bench.Instance.AddUnit(placedUnit.gameObject);
+        return;
+    }
+
+    GameManager.Instance.RemoveUnit(unit1);
+    GameManager.Instance.RemoveUnit(unit2);
+
+    var temp = unit1.gridPos;
+    GameManager.Instance.AddUnit(unit2.gridPos, unit1.gameObject);
+    GameManager.Instance.AddUnit(temp, unit2.gameObject);
+
+    unit1.transform.position = new Vector3(unit1.gridPos.x, unit1.gridPos.y);
+    unit2.transform.position = new Vector3(unit2.gridPos.x, unit2.gridPos.y);
  }
 
  Vector3 GetCurrentMousePos() {
